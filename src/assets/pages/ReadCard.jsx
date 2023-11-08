f50e811eae80d5c3f057982c1c594bef6e309ee3
Tonyliu2004 <ltsplice@gmail.com>
2023-11-08 16:02:38 -0500

    search name works

--- src/assets/pages/ReadCard.jsx
+++ src/assets/pages/ReadCard.jsx
@@ -5,20 +5,44 @@ import banner from '../images/lol-banner.jpg';
 
 const ReadCard = (props) =>{
     const [posts, setPosts] = useState([]);
+    const [searchText, setSearchText] = useState("");
+    const [searched, setSearched] = useState(false);
+    const [filtered, setFiltered] = useState(null);
     useEffect(() => {
         setPosts(props.data);
+        setFiltered(props.data);
     }, [props]);
 
+
+    const handleSearch = async (e) =>{
+        setSearched(true);
+        if(e != ""){
+            const f = Object.fromEntries(
+                Object.entries(props.data).filter(([, item]) =>
+                 item.title.toLowerCase().includes(searchText.toLowerCase())
+              )
+            );
+            setFiltered(f);
+        }
+    }
     return (
         <div>
             <div className = "title-bar">
                 <h1 className = "LOLtitle">League of Legends <br/>Discussion Hub</h1>
                 <img style = {{width: '100%', height: '100%'}} src = {banner} alt="League of Legends Banner image"></img>
             </div>
-            
+
+            <div className = "searchBar">
+                <input id = "searchInput" type="text" onChange={(inputString) => setSearchText(inputString.target.value)}/><br />
+                <button id = "searchButton" onClick = {handleSearch}>Search</button>
+            </div>
+
             <div className="readCards">
-                {
-                    posts && posts.length > 0 ?
+                { searched ?
+                    Object.values(filtered).map((post) => (
+                        <Card id={post.id} title={post.title} description={post.description} image={post.image} upvotes={post.upvotes} created_at = {post.created_at}/>
+                    ))
+                 :  posts && posts.length > 0 ?
                         posts.slice().reverse().map((post) => (
                             <Card id={post.id} title={post.title} description={post.description} image={post.image} upvotes={post.upvotes} created_at = {post.created_at}/>
                         ))
