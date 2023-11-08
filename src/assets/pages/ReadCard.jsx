c2acac1b460e0021df6051721b9bf5d6fa22d022
Tonyliu2004 <ltsplice@gmail.com>
2023-11-08 18:59:12 -0500

    did some formatting and added post count

--- src/assets/pages/ReadCard.jsx
+++ src/assets/pages/ReadCard.jsx
@@ -40,15 +40,25 @@ const ReadCard = (props) =>{
                 <img style = {{width: '100%', height: '100%'}} src = {banner} alt="League of Legends Banner image"></img>
             </div>
 
-            <div className = "searchBar">
-                <select style = {{marginRight:"5px  "}} id="sortOrderSelect" name="sortOrder" value={sortOrder} onChange = {(handleSortOrderChange)}>
-                    <option value = "time">Time</option>
-                    <option value = "upvotes-asc">Upvotes Ascending</option>
-                    <option value = "upvotes-desc">Upvotes Descending</option>
-                </select>
-
-                <input id = "searchInput" type="text" onChange={(inputString) => setSearchText(inputString.target.value)}/><br />
-                <button id = "searchButton" onClick = {handleSearch}>Search</button>
+            <div className = "searches">
+                <div>
+                    <label style = {{marginRight:"7px", fontSize:"16px"}}>Sort By</label>
+                    <select style = {{marginRight:"5px  "}} id="sortOrderSelect" name="sortOrder" value={sortOrder} onChange = {(handleSortOrderChange)}>
+                        <option value = "time">Time</option>
+                        <option value = "upvotes-asc">Upvotes Ascending</option>
+                        <option value = "upvotes-desc">Upvotes Descending</option>
+                    </select>
+                </div>
+                <p style = {{margin:"0px"}}> {
+                    searched ? 
+                    Object.keys(filtered).length
+                    : posts.length
+                
+                } results</p>
+                <div className = "searchBar">
+                    <input id = "searchInput" type="text" onChange={(inputString) => setSearchText(inputString.target.value)}/><br />
+                    <button id = "searchButton" onClick = {handleSearch}>Search</button>
+                </div>
             </div>
 
             <div className="readCards">
